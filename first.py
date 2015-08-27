654661a37d70efdd1d42fcce31abe68ddc7db91c
Hong SeongBong <stallon72@naver.com>
2015-08-27 18:37:25 +0900

    add printLargeInteger() func.

--- first.py
+++ first.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 __author__ = '홍성봉'
 
+from decimal import *
 
 s = 'module file sample'
 
 def add(a, b):
     return a+b
 
+
+def printLargeInteger():
+    large_integer = 2**3**4
+    print('Large Integer = %d' % large_integer)
+
+
 def main():
     print(add(3,4))
+    printLargeInteger()
+    printStr("stallon")
+
 
 
 if __name__ == '__main__':
