9264a19be49d6589f6a4ab5a1ef4ed98a18efd11
Hong SeongBong <stallon72@naver.com>
2015-08-31 10:19:15 +0900

    dfasdf

---
+++ strings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = '홍성봉'
+
+"""
+    This file is for an exercise for STRING chapter in Python Yeol-Hyeol-Gang-Ui
+"""
+
+print('{0} -- {1}'.format('홍성봉', '010-9530-5755'))
+
+
